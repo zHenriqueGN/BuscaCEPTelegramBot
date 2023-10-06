c82e926884d2459e655ab98dab89978d526e1571
Kennedy Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-10-06 18:07:24 -0300

    feat: create address entity

---
+++ internal/entity/address.go
@@ -0,0 +1,14 @@
+package entity
+
+type Address struct {
+	CEP         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	UF          string `json:"uf"`
+	IBGE        string `json:"ibge"`
+	GIA         string `json:"gia"`
+	DDD         string `json:"ddd"`
+	SIAFI       string `json:"siafi"`
+}
